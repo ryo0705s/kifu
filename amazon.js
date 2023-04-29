448ad9872a83e23cee85dc29e92c09a7d301448b
ryo0705s <inspire.sasaki15@gmail.com>
2023-04-29 20:32:15 +0900

    feat: amazonログイン仮実装

---
+++ amazon.js
@@ -0,0 +1,17 @@
+<>
+<div id="amazon-root"></div>
+
+
+ <script type="text/javascript">
+
+    window.onAmazonLoginReady = function() {
+      amazon.Login.setClientId('YOUR-CLIENT-ID')
+    };
+    (function(d) {
+      var a = d.createElement('script'); a.type = 'text/javascript';
+      a.async = true; a.id = 'amazon-login-sdk';
+      a.src = 'https://assets.loginwithamazon.com/sdk/na/login1.js';
+      d.getElementById('amazon-root').appendChild(a);
+    })(document);
+
+</script></>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <div id="amazon-root"></div>
   <v-row align-content="center">
     <v-col align="center">
       <v-list>
@@ -20,11 +21,57 @@
           <v-text-field label="password"></v-text-field> </v-form
       >
       </v-sheet> -->
-      <v-btn> <nuxt-link to="/">login</nuxt-link> </v-btn>
+      <!-- <v-btn> <nuxt-link to="/">login</nuxt-link> </v-btn> -->
+      <a href id="LoginWithAmazon">
+        <img
+          border="0"
+          alt="Login with Amazon"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
+          width="156"
+          height="32"
+          @click="handleClick()"
+        />
+      </a>
       <div>本アプリはamazonIDが必用になります</div>
       <v-btn><nuxt-link to="/contact">問い合わせ</nuxt-link> </v-btn>
     </v-col>
   </v-row>
 </template>
-<script setup lang="ts"></script>
+<script type="text/javascript">
+export default {
+  mounted() {
+    window.onAmazonLoginReady = function () {
+      amazon.Login.setClientId(
+        "amzn1.application-oa2-client.c4db0a0cfc104ecbb0afc2afc513f6c1"
+      );
+    };
+    (function (d) {
+      var a = d.createElement("script");
+      a.type = "text/javascript";
+      a.async = true;
+      a.id = "amazon-login-sdk";
+      a.src = "https://assets.loginwithamazon.com/sdk/na/login1.js";
+      d.getElementById("amazon-root").appendChild(a);
+    })(document);
+  },
+  setup() {
+    const handleClick = () => {
+      let options = {};
+      options.scope = "profile";
+      console.log(options, "options");
+      options.scope_data = {
+        profile: { essential: false },
+      };
+      amazon.Login.authorize(
+        options,
+        "http://localhost:3000//handle_login.php"
+      );
+      return false;
+    };
+    return {
+      handleClick,
+    };
+  },
+};
+</script>
 <style></style>
