29c8b709dc4108699e9e1c62c5796175dbc88158
ryo0705s <inspire.sasaki15@gmail.com>
2023-06-03 20:22:48 +0900

    feat: 寄付額をバックエンドに送れるように仮実装

--- pages/kifu/create.vue
+++ pages/kifu/create.vue
@@ -14,26 +14,43 @@
 
         <v-list-item
           ><v-sheet width="300"
-            ><v-text-field label="金額"></v-text-field>
+            ><v-text-field
+              label="金額"
+              type="number"
+              v-model="state.donation"
+            ></v-text-field>
             <div>~</div>
             <v-text-field label="金額"></v-text-field
           ></v-sheet>
           <div>の間</div>
         </v-list-item>
-        <v-list-item>この条件で検索</v-list-item>
+        <v-list-item
+          ><v-btn @click="addDonation">この条件で検索</v-btn>
+        </v-list-item>
       </v-list>
     </v-col>
   </v-row>
 </template>
 <script>
-export default {
-  // const state = reactive({
-  //   // ここにstateを定義する
-  //   isRandom: false,
-  //   return {
-  //     state
-  //     // ここにcomputedを定義する
-  //   };
-  // });
-};
+import { defineComponent, reactive } from "@vue/composition-api";
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      donation: 0,
+    });
+    const addDonation = async () => {
+      const { data } = await useFetch("http://127.0.0.1:8000/api/1", {
+        method: "POST",
+        body: JSON.stringify({
+          donation: state.donation,
+        }),
+      });
+      console.log(state.donation, "donation");
+    };
+    return {
+      state,
+      addDonation,
+    };
+  },
+});
 </script>
